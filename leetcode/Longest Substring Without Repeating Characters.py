da4a1554b023b6587bd50721d0200de8612776ca
bldev2473 <34266212+bldev2473@users.noreply.github.com>
2021-03-11 16:49:13 +0900

    Update Longest Substring Without Repeating Characters.py

--- leetcode/Longest Substring Without Repeating Characters.py
+++ leetcode/Longest Substring Without Repeating Characters.py
@@ -1,32 +1,80 @@
+'''
+#Time Limit Exceeded
 from collections import namedtuple
 LLS = namedtuple('LLS', ['str', 'len', 'result'])
 
 class Solution:
-    def __init__(self):
-        self.memoization_dict = {}
-    
-    def lengthOfLongestSubstring(self, s):
-    	for i in range(1, len(s) + 1):
-    		for j in range(len(s) + 1 - i):
-    			print(s)
-    			print('solve: ', self.solve(s[j:j + i]))
-    	print(self.memoization_dict)
-    	return max(self.memoization_dict.values())
-    def solve(self, s):
-    	if self.memoization_dict.get(s) is not None:
-    		self.memoization_dict.get(s)
-    		return LLS(s, len(s), True)
-    	if len(s) == 1:
-    		self.memoization_dict[s] = 1
-    		return LLS(s, len(s), True)
-    	else:
-    		if len(set(s)) == len(s):
-    			self.memoization_dict[s] = len(s)
-    			return LLS(s, len(s), True)
-    		else:
-    			return LLS(s, len(s), False)
-    				
+	def __init__(self):
+		self.memoization_dict = {}
+
+	def lengthOfLongestSubstring(self, s):
+		if len(s) <= 1:
+			return len(s)
+		for i in range(1, len(s) + 1):
+			for j in range(len(s) + 1 - i):
+				#print(s)
+				#print('solve: ', self.solve(s[j:j + i]))
+				self.solve(s[j:j + i])
+		#print(self.memoization_dict)
+		return max(self.memoization_dict.values())
+
+	def solve(self, s):
+		if self.memoization_dict.get(s) is not None:
+			self.memoization_dict.get(s)
+			return LLS(s, len(s), True)
+		if len(s) <= 1:
+			self.memoization_dict[s] = len(s)
+			return LLS(s, len(s), True)
+		else:
+			if len(set(s)) == len(s):
+				self.memoization_dict[s] = len(s)
+				return LLS(s, len(s), True)
+			else:
+				return LLS(s, len(s), False)
+
 sln = Solution()
 print(sln.lengthOfLongestSubstring('bbbbbbb'))
 print(sln.lengthOfLongestSubstring('abcabcbb'))
 print(sln.lengthOfLongestSubstring('pwwkew'))
+print(sln.lengthOfLongestSubstring(''))
+'''
+
+from collections import deque
+
+class Solution:
+    def __init__(self):
+        self.memoization_dict = {}
+
+    def lengthOfLongestSubstring(self, s):
+        dq = deque(maxlen=len(s))
+        if len(s) <= 1:
+            return len(s)
+        for i in range(0, len(s)):
+            dq.append(s[i]);
+            # print(dq)
+            if self.solve(dq) is not True:
+                dq.popleft()
+                continue
+            else:
+                continue
+        return max(self.memoization_dict.values())
+
+    def solve(self, dq):
+        m_list = ''.join(list(dq))
+        if self.memoization_dict.get(m_list) is not None:
+            return True
+        if len(m_list) <= 1:
+            self.memoization_dict[m_list] = len(m_list)
+            return True
+        else:
+            if len(set(m_list)) == len(m_list):
+                self.memoization_dict[m_list] = len(m_list)
+                return True
+            else:
+                return False
+
+sln = Solution()
+print(sln.lengthOfLongestSubstring('bbbbbbb'))
+print(sln.lengthOfLongestSubstring('abcabcbb'))
+print(sln.lengthOfLongestSubstring('pwwkew'))
+print(sln.lengthOfLongestSubstring(''))
